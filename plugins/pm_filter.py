e9d5de88b4ac5c98d780d2760c1947505fce13cb
illuzX <98640218+illuzX@users.noreply.github.com>
2022-05-24 11:16:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -36,8 +36,8 @@ async def give_filter(client, message):
     if k == False:
         await auto_filter(client, message)
 
-@Client.on_callback_query(filters.regex(r"^next"))
-async def next_page(bot, query):
+@Client.on_callback_query(filters.regex(r"^key"))
+async def key(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
         return await query.answer("Bruh please ask you're request don't be lezzy", show_alert=True)
